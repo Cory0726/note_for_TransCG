ba2c930c31a39aab22e1afa3b88e4d40e59bb9eb
Cory0726 <lkfu520@gmail.com>
2025-11-24 15:40:05 +0800

    update

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ import numpy as np
 
 
 if __name__ == "__main__":
-    raw_depth = cv2.imread("test_img/M1_01_intensity_adjusted.png", cv2.IMREAD_UNCHANGED)
-    print(np.shape(raw_depth), np.max(raw_depth), np.min(raw_depth))
+    raw_depth_mm = np.load("test_img/M1_01_raw_depth.npy").astype(np.uint16)
+    print(raw_depth_mm.shape, raw_depth_mm.max(), raw_depth_mm.min())
+
+    cv2.imwrite("test_img/M1_01_raw_depth_mm.png", raw_depth_mm)
+    check_depth_mm = cv2.imread("test_img/M1_01_raw_depth_mm.png", cv2.IMREAD_UNCHANGED)
+    print(check_depth_mm.shape, check_depth_mm.max(), check_depth_mm.min())
