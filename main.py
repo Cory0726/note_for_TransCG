f92728de2da18f4997250cf25589002905770610
Cory0726 <lkfu520@gmail.com>
2025-11-25 11:23:48 +0800

    update

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     raw_depth_mm = np.load('test_img/M1_01_raw_depth.npy')
     print('raw_depth_mm : ' + array_info(raw_depth_mm))
     # Grayscale image
-    grayscale_img = cv2.imread('test_img/M1_01_intensity_image.png', cv2.IMREAD_GRAYSCALE).astype(np.float32)
+    grayscale_img = cv2.imread('test_img/M1_01_intensity_darken05.png', cv2.IMREAD_GRAYSCALE).astype(np.float32)
     print('grayscale_img : ' + array_info(grayscale_img))
 
     # Mask
@@ -25,9 +25,13 @@ def main():
     # cv2.imshow('mask_raw_depth_mm_heatmap', mask_raw_depth_mm_heatmap)
     # cv2.waitKey(0)
 
-    # Convert depth unit mm to m
+    # Convert depth unit mm to m (mask_raw_depth_mm)
     mask_raw_depth_m = mask_raw_depth_mm / 1000
     print('mask_raw_depth_m : ' + array_info(mask_raw_depth_m))
+
+    # Convert depth unit mm to m (raw_depth_mm)
+    raw_depth_m = raw_depth_mm / 1000
+    print('raw_depth_m : ' + array_info(raw_depth_m))
 
     # Create a fake RGB image by grayscale image
     fake_rgb = np.stack([grayscale_img, grayscale_img, grayscale_img], axis=2)  # (H, W, 3)
@@ -43,26 +47,16 @@ def main():
     # # Call inferencer for refined depth
     depth_refine, depth_ori = inferencer.inference(
         rgb=norm_fake_rgb,
-        depth=mask_raw_depth_m,
+        depth=raw_depth_m,
         target_size=(640, 480),
-        depth_coefficient=10.0,
-        inpainting=True
+        depth_coefficient=5.0,
+        inpainting=True,
     )
     print('depth_refine : ' + array_info(depth_refine))
     depth_refine_heatmap = rawdepth_to_heatmap(depth_refine)
     print('depth_refine_heatmap : ' + array_info(depth_refine_heatmap))
-    # cv2.imwrite('result_img/M1_01_SegHand_depth_refine_heatmap.png', depth_refine_heatmap)
+    cv2.imwrite('result_img/M1_01d05_depth_refine_heatmap.png', depth_refine_heatmap)
     cv2.imshow('depth_refine_heatmap', depth_refine_heatmap)
     cv2.waitKey(0)
-    # raw_depth_m = (raw_depth_m * 1000).astype(np.uint16)
-    # print(raw_depth_m.shape, raw_depth_m.max(), raw_depth_m.min())
-    # raw_depth_m_heatmap = tools.rawdepth_to_heatmap(raw_depth_m)
-    # depth_refine = (depth_refine * 1000).astype(np.uint16)
-    # print(depth_refine.shape, depth_refine.max(), depth_refine.min())
-    # depth_refine_heatmap = tools.rawdepth_to_heatmap(depth_refine)
-    # cv2.imwrite('result_img/M11_1_raw_depth_heatmap.png', raw_depth_m_heatmap)
-    # cv2.imwrite('result_img/M1_11_depth_refine.png', depth_refine)
-    # cv2.imwrite('result_img/M1_11_depth_refine_heatmap.png', depth_refine_heatmap)
-
 if __name__ == "__main__":
     main()
